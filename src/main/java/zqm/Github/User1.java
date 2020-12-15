b78cbf6c075b433a21ab911c875086f722d43306
User1 <839854231@qq.com>
2020-12-15 19:07:14 +0800

    创建用户1

---
+++ src/main/java/zqm/Github/User1.java
@@ -0,0 +1,9 @@
+package zqm.Github;
+
+public class User1 {
+    public static void main(String[] args) {
+        System.out.println("你是帅比！！");
+        System.out.println("你是首富！！");
+        System.out.println("你是狠人！！");
+    }
+}
